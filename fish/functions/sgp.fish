ac5df5fb51213645e62b88d5fe6195ae5aa2879c
Benjamin Pollack <benjamin@bitquabit.com>
2013-08-27 16:02:55 -0400

    fish: update sgp to remove existing paths

--- fish/functions/sgp.fish
+++ fish/functions/sgp.fish
@@ -1,4 +1,14 @@
 function sgp -d "Set the GOPATH to the current directory, and add its /bin to the PATH"
+    if [ -n $GOPATH ]
+        for x in (seq (count $PATH))
+            if [ $PATH[$x] = "$GOPATH/bin" ]
+                set clear $x $clear
+            end
+        end
+        for x in $clear
+            set -e PATH[$x]
+        end
+    end
     set -gx GOPATH (pwd)
     set -x PATH $GOPATH/bin $PATH
 end
