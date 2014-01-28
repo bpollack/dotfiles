1dbf52ece3b04ebcbb952f56779fbb866fd39985
Benjamin Pollack <benjamin@bitquabit.com>
2014-01-28 16:46:31 -0500

    fish: do not crash if ~/.kerrit/users.tsv is missing

--- fish/completions/kerrit.fish
+++ fish/completions/kerrit.fish
@@ -1,7 +1,11 @@
 # fish completions for kerrit
 
 function __kerrit_users
+    set -l kerrit_users ~/.kerrit/users.tsv
+    if test ! -f $kerrit_users
+        return
+    end
     cut -f 2 ~/.kerrit/users.tsv | grep '@knewton.com$' | sed 's/@knewton.com$//'
 end
 
-complete -f -c kerrit -n 'commandline -pc | grep -q \'^ *kerrit  *r  *s  *\'' -a '(__kerrit_users)' -d 'Open a review of a commit'
+complete -f -c kerrit -n 'commandline -pc | grep -q \'^ *kerrit  *r  *s  *\'' -a '(__kerrit_users)'
