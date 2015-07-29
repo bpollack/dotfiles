faf592f72c8e1d9eddd4e1df690cb0aa122aca6d
Benjamin Pollack <benjamin@bitquabit.com>
2015-07-29 10:38:06 -0700

    fish: add Khan utilities, if I'm on a Khan computer

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,12 @@ end
 
 set -x PATH ~/.local/bin /usr/local/bin $PATH
 
+if test -d ~/khan
+    # Add frankenserver bins to PATH
+    set KDR ~/khan/devtools
+    set -x PATH ~/khan/webapp/third_party/frankenserver $KDR/arcanist/khan-bin $KDR/git-bigfile/bin $KDR/git-workflow/bin $KDR/ka-clone/bin $KDR/khan-linter/bin $PATH
+end
+
 # Python magic
 eval (python -m virtualfish)
 eval (python -m virtualfish auto_activation)
