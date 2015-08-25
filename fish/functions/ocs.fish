16328377df5b2d64ed4f006dfc8fb751ad561db7
Benjamin Pollack <benjamin@bitquabit.com>
2015-08-25 09:15:45 -0400

    fish: remove ocs alias, which I never use

--- fish/functions/ocs.fish
+++
@@ -1,6 +0,0 @@
-function -d "switches the active OCaml version and activates it" ocs
-    opam switch --no-warning $argv
-    eval (opam config --fish env)
-    set -x MANPATH /usr/share/man /usr/local/share/man $MANPATH
-end
-
