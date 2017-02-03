629c0cbdc35cc256eff7c2084e3d5cb12335a7ac
Benjamin Pollack <benjamin@bitquabit.com>
2017-02-02 20:37:22 -0500

    powershell: ditch unused Set-Link function

--- install.ps1
+++ install.ps1
@@ -1,16 +1,5 @@
 $dotfiles = Split-Path $MyInvocation.MyCommand.Path
 
-function Set-Link([string]$source, [string]$target, [bool]$directory = $false) {
-    if ($directory) {
-        $flag = "/d"
-    } else {
-        $flag = ""
-    }
-    if (-not (Test-Path $target)) {
-        cmd /c mklink $flag $target $dotfiles\$source
-    }
-}
-
 function Set-Directory([string]$dir) {
     if (-not (Test-Path $dir)) {
         mkdir -force $dir
