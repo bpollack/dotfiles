8ca545ca66e9c5cdfe2e109e1475249a2ca41522
Benjamin Pollack <benjamin@bitquabit.com>
2017-08-16 19:13:32 -0400

    fish: numerous small twekas, including adding opam

--- fish/config.fish
+++ fish/config.fish
@@ -11,38 +11,43 @@ function fish_greeting
     end
 end
 
-set -x PATH ~/.local/bin /usr/local/bin $PATH
+set -gx PATH ~/.local/bin /usr/local/bin $PATH
 
 # If we're on a Mac, we probably have zulu; add it
 set zulu_path ~/.local/zulu/bin
 if test -d $zulu_path
-    set -x PATH $zulu_path $PATH
-    set -x JAVA_HOME $zulu_path
+    set -gx PATH $zulu_path $PATH
+    set -gx JAVA_HOME $zulu_path
 end
 
 # On Macs, we have Go installed custom; add it to path if present
 if test -d /usr/local/go
-    set -x PATH /usr/local/go/bin $PATH
+    set -gx PATH /usr/local/go/bin $PATH
 end
 
 # Go root
 if test -d ~/src/goroot
-    set -x GOPATH ~/src/goroot
-    set -x PATH ~/src/goroot/bin $PATH
+    set -gx GOPATH ~/src/goroot
+    set -gx PATH ~/src/goroot/bin $PATH
 end
 
 # For Khan Academy!
 if test -d ~/khan
     # Add frankenserver bins to PATH
     set KDR ~/khan/devtools
-    set -x PATH ~/khan/webapp/third_party/frankenserver $KDR/arcanist/khan-bin $KDR/git-bigfile/bin $KDR/git-workflow/bin $KDR/ka-clone/bin $KDR/khan-linter/bin $PATH
+    set -gx PATH ~/khan/webapp/third_party/frankenserver $KDR/arcanist/khan-bin $KDR/git-bigfile/bin $KDR/git-workflow/bin $KDR/ka-clone/bin $KDR/khan-linter/bin $PATH
 end
 
 # Python magic
-eval (python -m virtualfish)
-eval (python -m virtualfish auto_activation)
-set -x SHELL /usr/local/bin/fish
-set -x EDITOR kak
+eval (python2 -m virtualfish)
+eval (python2 -m virtualfish auto_activation)
+
+# OPAM configuration
+. /Users/benjamin/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
+
+# General
+set -gx SHELL /usr/local/bin/fish
+set -gx EDITOR nvim
 
 if [ (uname -a | grep Microsoft) ]
     fundle plugin 'tuvistavie/fish-ssh-agent'
