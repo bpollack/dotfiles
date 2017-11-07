f6d92821106763b3eea6de2dda22c8d58b834245
Benjamin Pollack <benjamin@bitquabit.com>
2017-11-06 20:06:48 -0500

    add script to install Homebrews

---
+++ homebrew.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+brew install \
+    elixir \
+    fd \
+    fish \
+    git \
+    go \
+    httpie \
+    hugo \
+    jq \
+    mosh \
+    neovim \
+    node \
+    packer \
+    python \
+    python3 \
+    rbenv \
+    ripgrep \
+    rust \
+    task \
+    tasksh \
+    terraform \
+    yarn
