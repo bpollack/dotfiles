2b48401e87da25c8f8b6be103083b6a37dd5f127
Benjamin Pollack <benjamin@bitquabit.com>
2018-03-29 15:47:51 -0400

    fish: remove plan9port again
    The way I wrote this path injection, Plan 9 keeps ending up *before* other path
    components in Fish shell, which breaks everything. I'm sure there's an easy
    fix, but I need to acutally sit down and write it before plan9port can be
    automatically added to the path.

--- fish/config.fish
+++ fish/config.fish
@@ -32,12 +32,6 @@ if test -f '/Users/benjamin/.local/google-cloud-sdk/path.fish.inc'
     end
 end
 
-# Plan 9 root
-if test -d ~/src/plan9
-    set -x PLAN9 ~/src/plan9
-    set -x PATH $PATH ~/src/plan9/bin
-end
-
 # General
 set -x SHELL /usr/local/bin/fish
 set -x EDITOR nvim
