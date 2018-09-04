4b5d24416ff860179196009b8f30e3320c3c5f9e
Benjamin Pollack <benjamin@bitquabit.com>
2018-09-04 15:27:07 -0400

    homebrew: clean up dependencies

--- homebrew.sh
+++ homebrew.sh
@@ -3,20 +3,15 @@
 brew cask install docker gitup zulu
 brew upgrade
 brew install \
-    dep \
     fd \
     fish \
     git \
-    go \
     httpie \
-    hugo \
     jq \
     minio \
     minio-mc \
     mosh \
     neovim \
-    python \
-    python3 \
     ripgrep \
     tmux \
     yarn
